3fcea6a22f43ad0f66a59d6108d149cbb81d485a
Maithrishetty3 <maithri@specbee.com>
2018-11-25 17:53:43 +0530

    function

--- dbFunc.php
+++ dbFunc.php
@@ -1,119 +1,72 @@
-<?php  
-session_start();  
-require_once 'dbConnect.php';  
+<?php
+class db{
+  private $server = "localhost";
+  private $dbuser = "root";
+  private $dbpassword = "root";
+  private $dbname="Travel";
+  public $conn;
 
-    class dbFunc {  
-        private $db;
-        public function __construct() {  
-            $this->db = new dbConnect();   
-        }  
-    
-        function Insertdata($table,$field,$data)
-        {
-            $field_values= implode(',',$field);
-            $data_values=implode("','",$data);
-        
-            $sql= "INSERT INTO $table (".$field_values.")
-        VALUES ('".$data_values."') ";
-            $result = mysqli_query($this->db->conn,$sql);
-            if($result)
-            {
-            }
-            else
-            {
-                echo "Error: " . $sql . "" . mysqli_error($this->db->conn);
-                exit(1);
-            }
-        }
-        function fetch_comment($sid)
-        {
-            $sql = " Select id,sid,name,body,date from comments where sid = $sid order by  id desc  ;";
-            $result = mysqli_query($this->db->conn, $sql);
-            if($result)
-            {
-                while($row = mysqli_fetch_assoc($result))
-                {
-//                    print_r($row);die;
-                  $solutions[$row['id']] = $row;
-                }
-            return $solutions;
-            }
-            else
-            {
-                echo "Error: " . $sql . "" . mysqli_error($this->db->conn);
-                exit(1);
-            }
-        }
+  public function dbconnector()  {
+      $this->conn = new mysqli($this->server,$this->dbuser,$this->dbpassword,$this->dbname);
+      return $this->conn;
+  }
 
-        function reply_comment($pid)
+  function update_data($table_name, $form_data,$story_id)
         {
-            $sql = " Select * from reply where pid = $pid order by  id desc;";
-            $result = mysqli_query($this->db->conn, $sql);
-            if($result)
-            {
-                while($row = mysqli_fetch_assoc($result))
-                {
-                    $solutions[$row['id']] = $row;
-                }
-                return $solutions;
-            }
-            else
-            {
-                echo "Error: " . $sql . "" . mysqli_error($this->db->conn);
-                exit(1);
-            }
+        $valueSets = array();
+        foreach($form_data as $key => $value) {
+        $valueSets[] = $key . " = '" . $value . "'";
         }
+        $sql = "UPDATE $table_name SET ". join(",",$valueSets) . " WHERE story_id = '".$story_id."'";
+        $result = mysqli_query($this->conn, $sql);
+        if (mysqli_query($this->conn, $sql)) {
+         
+        }
+        else
+        {
+        echo "Error: " . $sql . "" . mysqli_error($conn);
+        }
+      }
+
+      function delete_image($table_name, $form_data,$story_id,$travel_id)
+        {
+        $valueSets = array();
+        foreach($form_data as $key => $value) {
+        $valueSets[] = $key . " = '" . $value . "'";
+        }
+        $sql = "UPDATE $table_name SET ". join(",",$valueSets) . " WHERE story_id = '".$story_id."'";
+        $result = mysqli_query($this->conn, $sql);
+        if (mysqli_query($this->conn, $sql)) {
+         header('location:delete_images.php?travel_id=' . $travel_id);
+        }
+        else
+        {
+        echo "Error: " . $sql . "" . mysqli_error($conn);
+        }
+      }
+
+      function delete_user($table_name, $form_data,$story_id,$travel_id)
+        {
+        $valueSets = array();
+        foreach($form_data as $key => $value) {
+        $valueSets[] = $key . " = '" . $value . "'";
+        }
+        $sql = "UPDATE $table_name SET ". join(",",$valueSets) . " WHERE story_id = '".$story_id."'";
+        $result = mysqli_query($this->conn, $sql);
+        if (mysqli_query($this->conn, $sql)) {
+         header('location:delete_images.php?travel_id=' . $travel_id);
+        }
+        else
+        {
+        echo "Error: " . $sql . "" . mysqli_error($conn);
+        }
+      }
 
 
-        function like_check($sid,$user,$select)
-        {
-            $sql = " Select $select from reaction where sid = $sid and user = '$user'";
-            $result = mysqli_query($this->db->conn, $sql);
-            if($result)
-            {
-                if($row = mysqli_fetch_assoc($result))
-                {
-                    $solutions = $row;
-                }
-                return $solutions;
-            }
-            else
-            {
-                echo "Error: " . $sql . "" . mysqli_error($this->db->conn);
-                exit(1);
-            }
-        }
+      
 
-        function like_update($sid,$user,$like,$dislike)
-        {
-            $sql = "UPDATE reaction SET `like` = $like, dislike = $dislike  WHERE  sid = $sid and user =  '$user'";
-            $result = mysqli_query($this->db->conn, $sql);
-            if($result)
-            {
-                return true;
-            }
-            else
-            {
-                echo "Error: " . $sql . "" . mysqli_error($this->db->conn);
-                exit(1);
-            }
-        }
+     
 
-        function like_count($sid,$user,$check)
-        {
-            $sql = "select count(id) from reaction where $check = '1' and sid= $sid";
-            $result = mysqli_query($this->db->conn, $sql);
-            if($result)
-            {
-                $row = mysqli_fetch_assoc($result);
-                $solutions = $row['count(id)'];
-                return $solutions;
-            }
-            else
-            {
-                echo "Error: " . $sql . "" . mysqli_error($this->db->conn);
-                exit(1);
-            }
-        }
-    }
-?>  
+}
+
+?>

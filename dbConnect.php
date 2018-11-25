a05686daaed0a4ecf1cd22b1ca0b78ae4ff6ce46
DeekshaPrabhuB <deeksha@specbee.com>
2018-11-25 22:47:36 +0530

    story added

--- dbConnect.php
+++ dbConnect.php
@@ -2,7 +2,7 @@
     class dbConnect {  
         public $conn;
         function __construct() {  
-            $this->conn = new mysqli("localhost", "root", "Abcd@123", "Travel");
+            $this->conn = new mysqli("localhost", "root", "Abcd@123", "comments");
             if ($this->conn->connect_error) {
                 die("Connection failed: " . $this->conn->connect_error);
             }
--- details.php
+++ details.php
@@ -244,6 +244,6 @@ $row = $obj-> fetch_comment($sid);
     </div>
     </body>
     <script src='https://www.google.com/recaptcha/api.js'></script>
-    </html>?>
+    </html>
     </body>
 </html>
